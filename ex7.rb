f4ae1909de77d95002a6680527c851768bb8f564
Varvara <shinkarenko.varvara@gmail.com>
2016-11-30 12:11:44 +0000

    Ex-7

---
+++ ex7.rb
@@ -0,0 +1,26 @@
+# prints first sentence
+puts "Mary had a little lamb."
+# prints decond sentence on a new line
+puts "Its fleece was white as #{'snow'}."
+# prints third sentence on a next line
+
+puts "And everywhere that Mary went."
+# prints string "." 10 times
+puts "." * 10
+# new variables end1, end2, ...end12 - each are assigned a short string value
+end1 = "C"
+end2 = 'h'
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = 'e'
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+# use print function to print out the sum of first 5 strings
+print end1 +end2+end3+end4+end5+end6
+#use puts function instead of print as it will add a new line to the end of the program
+puts end7 +end8 + end9 + end10 + end11 +end12
