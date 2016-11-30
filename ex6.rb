9639711590e7e342980ae228cc19c1f3044c46e5
Varvara <shinkarenko.varvara@gmail.com>
2016-11-30 11:49:36 +0000

    Ex-6

---
+++ ex6.rb
@@ -0,0 +1,30 @@
+# declare a viriable called types_of_people and assign a number of people
+types_of_people  = 10
+# new variable x is assigned to a string value that includes that variable types_of_people
+x = "There are #{types_of_people} types of people."
+# assign a string to a binary variable
+binary ="binary"
+# assignig a string to a new variable do_not
+do_not = "don't"
+# create new variable - "y", and assign to a string which include two other strings
+y = "Those who know #{binary} and those who #{do_not}."
+#prints the resulting string of a variable x
+puts x
+#prints the resulting string of a variable y
+puts y
+#prints string x inside another string
+puts "I said: #{x}."
+#prints string y inside another string
+puts "I also said: '#{y}'."
+# assigns false value to the new variable
+hilarious = false
+# assigns string value to the new variable joke_evaluation, and inserts another var inside the string
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+# prints the string assigned to a value joke_evaluation
+puts joke_evaluation
+# creates new variable and assighn a string to it
+w = "This is the left side of..."
+# creates new variable and assighn a string to it
+e = "a string with a right side."
+# prints the sum of two variables - adds two strings inside them and makes them one
+puts w + e
