8e9ca595f2809e3ddd2fcd56ac4946fa5d0a26fe
Varvara <shinkarenko.varvara@gmail.com>
2016-11-30 10:23:16 +0000

    Ex-3

---
+++ ex3-a.rb
@@ -0,0 +1,4 @@
+puts "Count the area of the room"
+puts "Length = 5.0"
+puts "Width =4.5"
+puts "area = 5*4.5 = #{5.0*4.5}" 
---
+++ ex3.rb
@@ -0,0 +1,28 @@
+# puts the sentence stating what the next step will do
+puts "I will now count my chickens:"
+# PUts the string with the number of Hens, and performs counting
+puts "Hens #{25.0+30.0/6.0}"
+# counts a number of roosters inside the string
+puts "roosters #{100.0- 25.0*3.0%4.0}"
+# state what the next step will be - counting eggs
+puts "Now I will count the eggs:"
+# counts number of eggs
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+# asks if the arithmetic statement inside the string is true or false
+puts "Is it true that 3 + 2 < 5 - 7?"
+# it checks if the arithmetic statement is true or false and prints the answer
+puts 3.0 + 2.0 < 5.0 - 7.0
+# on the same line asks what is the sum of two numbers and prints an answer
+puts "What is 3 + 2? #{3.0 + 2.0}"
+# on the same line asks what is the difference between two numbers and counts it
+puts "What is 5 - 7? #{5.0 - 7.0}"
+# prints a phrase showing that person understands why the above statement is false
+puts "Oh, that's why it's false."
+# suggests to have some more arithemtic comparisons to try out
+puts "How about some more."
+# asks if the statement true or false and prints the answer
+puts "Is it greater? #{5 > -2}"
+# asks if the statement true or false and prints the answer
+puts "Is it greater or equal? #{5 >= -2}"
+# asks if the statement true or false and prints the answer
+puts "Is it less or equal? #{5 <= -2}"
